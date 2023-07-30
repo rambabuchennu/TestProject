6b7a6e085c228f31f42e4c4080eaebf42fe9aecf
rambabu.chennu <rambabu.chennu@gmail.com>
2023-07-30 21:00:12 +0530

    First commit.

---
+++ TestProject/src/ram/com/TestProgram.java
@@ -0,0 +1,9 @@
+package ram.com;
+
+public class TestProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" This is sample java program");
+	}
+}
